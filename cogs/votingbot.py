cb13af50fb63cff0e0d4845be27b2f2c1d13fa74
Nico Weidmann <nicoweidmann@web.de>
2017-08-15 17:19:22 +0200

    Remove unused statement, uncomment commented decorator
    Removed a left-over statement, uncommented a commented
    decorator that was disabled for debugging purposes.

--- cogs/votingbot.py
+++ cogs/votingbot.py
@@ -115,7 +115,7 @@ class Voting:
 
     @createVote.error
     @vote.error
-    # @evaluate.error
+    @evaluate.error
     @showVote.error
     @asyncio.coroutine
     def command_error(self, error, context):
@@ -177,7 +177,6 @@ class Voting_Model:
         totalVotes = len(self.votes)
         for i, a, v in sorted([(i, a, list(self.votes.values()).count(i)) for i, a in enumerate(self.answers)],
                               key=lambda tup: -tup[2]):
-            currentVotes = list(self.votes.values()).count(i)
             percentage = v / totalVotes if totalVotes != 0 else 0
             bar_length = int(percentage * 25)
             msg = (msg + a + ":\n" +
